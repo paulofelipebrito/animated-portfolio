1dd4f7757efec9dd2094d8406ea2e0eace53d1b6
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-07-07 09:22:06 -0300

    feat: Banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/Navbar";
+import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
