519163caf8516257c01f7491e69bd33c3d05a2ff
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-08-25 07:15:33 -0300

    non used var

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,7 +34,8 @@ export const Contact = () => {
       body: JSON.stringify(formDetails),
     });
     setButtonText("Send");
-    // let result = await response.json();
+    let result = await response.json();
+    console.log(result);
     setFormDetails(formInitialDetails);
     // if (result.code === 200) {
     //   setStatus({ succes: true, message: 'Message sent successfully'});
